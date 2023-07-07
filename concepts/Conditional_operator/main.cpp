d5fe21fd409c81c4df56dcf62ad980d02032e7b5
hsahni55h <h.sahni1998@gmail.com>
2023-07-07 23:38:52 +0200

    in dev

---
+++ concepts/Conditional_operator/main.cpp
@@ -0,0 +1,25 @@
+// Section 9
+// Conditional Operator
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1{}, num2{};
+    
+    cout << "Enter two integers separated by a space: ";
+    cin >> num1 >> num2;
+    
+    if (num1 != num2) {
+        cout << "Largest: " << ( (num1 > num2) ? num1 : num2 ) << endl;
+        cout << "Smallest: " << ( (num1 < num2) ? num1 : num2) << endl;
+        
+    } else {
+        cout << "The numbers are the same" << endl;
+    }
+    
+    cout  << endl;
+    return 0;
+}
+
