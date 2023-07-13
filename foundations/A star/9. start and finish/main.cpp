8b327489a9a71bb07db592079ca2c3543d0b8287
hsahni55h <h.sahni1998@gmail.com>
2023-07-13 16:51:04 +0200

    in dev

--- foundations/A star/9. start and finish/main.cpp
+++ foundations/A star/9. start and finish/main.cpp
@@ -190,16 +190,18 @@ void ExpandNeighbors(const vector<int> &current,  int goal[2], vector<vector<int
     int x1 = current[0];
     int y1 = current[1];
     int g1 = current[2];
-    int h1 = Heuristic(x1, y1, goal[0], goal[1]);
-    for (auto i : delta)
+    for (int i = 0; i < 4; i++)
     {
-        int x2 = x1 + i[0];
-        int y2 = y1 + i[1];
-        if(CheckValidCell(x2, y2, grid ))
+        int x2 = x1 + delta[i][0];
+        int y2 = y1 + delta[i][1];
+
+        // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+        if (CheckValidCell(x2, y2, grid))
         {
+            // Increment g value and add neighbor to open list.
             int g2 = g1 + 1;
             int h2 = Heuristic(x2, y2, goal[0], goal[1]);
-            AddToOpen(x2, y2, g2, h2, open, grid );
+            AddToOpen(x2, y2, g2, h2, open, grid);
         }
     }
 }
