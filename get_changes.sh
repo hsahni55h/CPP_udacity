b99f851969facefa0a89a9071c3c4c78c662e8af
himanshu <h.sahni1998@gmail.com>
2023-08-04 18:21:55 +0530

    commit

--- get_changes.sh
+++ get_changes.sh
@@ -1,10 +1,10 @@
-# in dev
-git stash
-git checkout main
+# # in dev
+# git stash
+# git checkout main
 
-# in main
-git fetch
-git pull
+# # in main
+# git fetch
+# git pull
 
-git checkout -b dev
-# in dev and repeat
+# git checkout -b dev
+# # in dev and repeat
